d7d74843977da18960711ead14dc59d64ae3e89d
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2021-07-20 19:12:17 -0300

    add more tests to phone

--- tests/Domain/PhoneTest.php
+++ tests/Domain/PhoneTest.php
@@ -31,4 +31,27 @@ class PhoneTest extends TestCase
 
         Phone::create($number);
     }
+
+    public function invalidPhones(): array
+    {
+        return [
+            ['(14)998116746'],
+            ['(14)99811-6746'],
+            ['14 998116746'],
+            ['14 99811-6746'],
+            [' (14) 99811-6746  ']
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidPhones
+     */
+    public function shouldntCreatePhoneIfIsFormattedWrong(string $number)
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('This phone has invalid format.');
+
+        Phone::create($number);
+    }
 }
