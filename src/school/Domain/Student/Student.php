cf5482d29400f7e205ca66cb3584f558db0966f8
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2021-07-21 12:44:18 -0300

    add registeredAt to student

--- src/school/Domain/Student/Student.php
+++ src/school/Domain/Student/Student.php
@@ -4,23 +4,28 @@ declare(strict_types=1);
 
 namespace School\Domain\Student;
 
+use DateTimeImmutable;
+use DateTimeInterface;
 use DomainException;
 
 class Student
 {
 
-    private $phones = [];
+    /** @var Phone[] $phones */
+    private array $phones = [];
+    private DateTimeInterface $registeredAt;
 
     public function __construct(
         private CPF $cpf,
         private string $name,
         private Email $email,
     ) {
+        $this->registeredAt = new DateTimeImmutable();
     }
 
     public function addPhone(Phone $phone): void
     {
-        if (count($this->phones) >= 2) {
+        if (count($this->phones) === 2) {
             throw new DomainException('You can\'t add more phones.');
         }
 
